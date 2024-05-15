2e078ae87df2c8a0be92a3378b2ca1db5dba648f
Kevin Hong <hong.w.kevin@gmail.com>
2024-05-15 13:32:06 -0700

    complete second challenge

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -1,9 +1,71 @@
 "use strict";
 
-// Print all entries, across all of the *async* sources, in chronological order.
+const { MinPriorityQueue } = require('@datastructures-js/priority-queue');
 
-module.exports = (logSources, printer) => {
-  return new Promise((resolve, reject) => {
-    resolve(console.log("Async sort complete."));
-  });
+const DEFAULT_MAX_HEAP_SIZE = 100000;
+const getLogEntryValue = ({ logEntry }) => logEntry.date;
+
+module.exports = async (logSources, printer, maxHeapSize = DEFAULT_MAX_HEAP_SIZE) => {
+  // heap item shape: { logEntry: { date: Date, msg: string}, logSourceIndex: string }
+  const minHeap = new MinPriorityQueue(getLogEntryValue);
+
+  // keep track of each log sources' "inventory"
+  const logSourceUnusedLogCount = [...Array(logSources.length)].map(() => 0);
+
+  // Maintain set of undrained log sources. This will increase performance. 
+  const undrainedLogSourceSet = new Set([...Array(logSources.length)].map((_, index) => index));
+
+  async function popAndEnqueueLogEntryBySourceIndex(logSourceIndex) {
+    const logEntry = await logSources[logSourceIndex].popAsync();
+
+    if (logEntry) {
+      minHeap.enqueue({ logEntry, logSourceIndex });
+      logSourceUnusedLogCount[logSourceIndex]++;
+    } else {
+      undrainedLogSourceSet.delete(logSourceIndex);
+    }
+  }
+
+  async function fetchNextLogEntriesBatch() {
+    // Ensure the space doesn't go out of control
+    if (minHeap.size() > maxHeapSize) {
+      return;
+    }
+
+    const promises = [];
+
+    const undrainedSourcesIndexes = undrainedLogSourceSet.values();
+
+    for (const index of undrainedSourcesIndexes) {
+      promises.push(popAndEnqueueLogEntryBySourceIndex(index))
+    }
+    
+    return Promise.all(promises);
+  }
+
+  // initial run to ensure each source has at least one log fetched
+  await fetchNextLogEntriesBatch();
+
+  while (!minHeap.isEmpty()) {
+    const { logEntry, logSourceIndex } = minHeap.dequeue();
+    logSourceUnusedLogCount[logSourceIndex]--;
+
+    printer.print(logEntry);
+
+    /**
+     * Enforce there's at least one entry fetched from each source
+     * This will dip the performance for a bit,
+     * but instead we dont need to worry about print speed > fetch speed 
+     * (especially when heap size is small)
+     */
+    while (!logSources[logSourceIndex].drained && logSourceUnusedLogCount[logSourceIndex] <= 0) {
+      await popAndEnqueueLogEntryBySourceIndex(logSourceIndex);
+    }
+
+    await fetchNextLogEntriesBatch();
+  }
+
+  printer.done();
+
+  return console.log("Async sort complete.")
 };
